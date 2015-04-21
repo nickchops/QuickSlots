84fef88b476a5b33857b7debe60718e1c31fe5d2
Nick <nickraymondsmith@gmail.com>
2015-04-21 10:42:11 +0100

    Update NodeUtility helper

--- resources/helpers/NodeUtility.lua
+++ resources/helpers/NodeUtility.lua
@@ -2,7 +2,6 @@
 -- Utility functions for working with Marmalade Quick Node objects
 -- (C) Nick Smith 2014
 
-
 function cancelTimersOnNode(node, recursive)
     for k,v in pairs(node.timers) do
         v:cancel()
@@ -13,10 +12,17 @@ function cancelTimersOnNode(node, recursive)
         end
     end
 end
+
 function cancelTweensOnNode(node, recursive)
-    for k,v in pairs(node.tweens) do
-        tween:cancel(v)
+    --Docs state to not loop though node.tweens manually, but we're doing it anyway!
+    --NB: in SDK, tween:cancel() causes tween to be removed from node.tweens in an ipairs loop
+    --We're relying on that being true - using our own ipairs loop here would cause errors.
+    --Prob should also update tween:cancel in SDK to call break one it removes a tween
+    --for a tiny bit of performance (or replace SDK's loop with index by id or somesuch).
+    while node.tweens[1] do
+        tween:cancel(node.tweens[1])
     end
+    
     if recursive then
         for k,child in pairs(node.children) do
             cancelTweensOnNode(child)
@@ -35,6 +41,7 @@ end
 function destroyNode(node)
     cancelTimersOnNode(node)
     cancelTweensOnNode(node)
+    if node.physics then physics:removeNode(node) end
     node:removeFromParent() --also sets parent's reference to this node to nil
     return nil -- calling myNode = destroyNode(myNode) sets ref to nil
                -- to match behaviour of node:removeFromParent()
@@ -69,7 +76,7 @@ function destroyNodesInTree(node, destroyRoot)
     --node in parent's .children array and uses table.remove.
     --For our loop, we can't use pairs() as order is not guaranteed. Can't use
     --ipairs as behaviour is undefined after table.remove during ipairs loop.
-    --So, we use a manual loop and know that the .remove call will collapse
+    --So, we use a manual loop and know that the SDK's .remove call will collapse
     --the tree meaning we don't need to increment the index.
     local i = 1
     while node.children[i] do
@@ -84,8 +91,8 @@ function destroyNodesInTree(node, destroyRoot)
     
     if destroyRoot then
         destroyNode(node)
-        return nil --allow user to call myNode = destroyNodesInTree(myNode, yes)
-                   --to also nil original reference
+        return nil --return nil allows user to call myNode = destroyNodesInTree(myNode, yes)
+                   --convenient way to nil reference and matches removeFromParent() behaviour
     end
     
     --we could probably make this more efficient by traversing the other
@@ -169,4 +176,104 @@ function UnFlickerFx(event)
     event.target:resumeTweens()
     event.target.flicker = false
     event.target:addTimer(FlickerFx, math.random(event.target.flickerMin,event.target.flickerMax)/10, 1)
+end
+
+--------------------------------------------------------------------
+-- scaling
+
+-- TODO: In SDK itself, we should attach these to the Node type so can just do myNode:setDefaultSize() etc
+
+-- Sprites are the size of the file image by default and xScale/yScale is realtive to that
+-- You cant just set w and h on a sprite, its always the file size.
+-- This allows you to set an absolute default size in pixels.
+-- Then you can use setRelativeScale to set x/y scales relative to the size you set here.
+function setDefaultSize(node, w, h, keepRatio, scaleNow)
+    if keepRatio == nil then keepRatio = true end --only used if only one of w or h is specified
+    if scaleNow == nil then scaleNow = true end  --apply scaling now. On by default
+    node.defaultScaleX = 1
+    node.defaultScaleY = 1
+    node.defaultW = w
+    node.defaultH = h
+    if w then
+        node.defaultScaleX = w/node.w
+    end
+    if h then
+        node.defaultScaleY = h/node.h
+    end
+    if keepRatio then
+        if node.defaultScaleY and not w then
+            node.defaultScaleX = node.defaultScaleY
+        end
+        if node.defaultScaleX and not h then
+            node.defaultScaleY = node.defaultScaleX
+        end
+    end
+    if scaleNow then
+        node.xScale = node.defaultScaleX
+        node.yScale = node.defaultScaleY
+    end
+end
+
+-- like above, but only one-off. setRelativeSize will ignore anything we set here
+function setSize(node, w, h, keepRatio)
+    if keepRatio == nil then keepRatio = true end --only used if only one of w or h is specified
+    local wScale
+    local hScale
+    if w then
+        wScale = w/node.w
+        node.xScale = wScale
+    end
+    if h then
+        hScale = h/node.h
+        node.yScale = hScale
+    end
+    if keepRatio then
+        if hScale and not wScale then
+            node.xScale = hScale
+        end
+        if wScale and not hScale then
+            node.yScale = wScale
+        end
+    end
+end
+
+function getScaledX(node, x)
+    if node.defaultScaleX then
+        return x*node.defaultScaleX
+    else
+        return x
+    end
+end
+
+function getScaledY(node, y)
+    if node.defaultScaleY then
+        return y*node.defaultScaleY
+    else
+        return y
+    end
+end
+
+function setRelativeScale(node, xScale, yScale)
+    if xScale then
+        if node.defaultScaleX then
+            node.xScale = xScale*node.defaultScaleX
+        else
+            node.xScale = xScale
+        end
+    end
+    if yScale then
+        if node.defaultScaleY then
+            node.yScale = yScale*node.defaultScaleY
+        else
+            node.yScale = yScale
+        end
+    end
+end
+
+function getWidth(node)
+    return node.w*node.xScale
+end
+
+function getHeight(node)
+    return node.h*node.yScale
 end
